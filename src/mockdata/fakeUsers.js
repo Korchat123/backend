49212579f8a8d26bd85aa23840d093e95709382d
Korchat123 <korchat123@gmail.com>
2026-05-13 15:16:31 +0700

    add git

---
+++ src/mockdata/fakeUsers.js
@@ -0,0 +1,13 @@
+export const users=[
+    {
+        id:1,
+        username:"korchat",
+        email:"korchat@hotmail.com",
+        password:"korkor123"
+    },
+    {  id:2,
+        username:"sun",
+        email:"sunt@hotmail.com",
+        password:"sun123"
+    }
+]
