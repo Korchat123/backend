0797e5bbab6ef82761a9b1f9d1225dd8f99e5912
Korchat123 <korchat123@gmail.com>
2026-05-14 16:02:29 +0700

    add delete

--- src/routes/v1/product.routes.js
+++ src/routes/v1/product.routes.js
@@ -1,5 +1,5 @@
 import { json, Router } from "express";
-import { products } from "../../mockdata/fakePoduct";
+import { products } from "../../mockdata/fakePoducts.js";
 export const router = Router();
 
 router.get("/",(req,res)=>{
@@ -8,17 +8,46 @@ router.get("/",(req,res)=>{
 
 })
 router.put("/:id",(req,res)=>{
-    
+    const product=products.find((e)=>(e.id===parseInt(req.params.id)))
+    if(product){
+
+            if(req.body.name&&req.body.description){
+                product.name=req.body.name;
+                product.description=req.body.description;
+            }else{
+                return(res.send('no Productname or description'))
+            }
+    }else{return(res.send('cant find product'))}
+
+    res.status(200).json(product);
+        
     
 
 })
 router.post("/",(req,res)=>{
-
-
+    const{name,description}=req.body;
+    if(name&&description)
+     {  
+        const id=products.length+1;
+        const product={id,name,description};   
+        products.push(product);
+        res.status(200).json(product)
+     }else{
+        res.send('data uncomplete pls check name and description')
+     }
 
 })
 router.delete("/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    // products=products.filter((e)=>{e.id!=id})
+  
+    
 
-
+      //  console.log(product)
+        if(req.params.id&&products)
+        {products.splice(id-1,1);
+        res.status(200).json(products);}
+    else{res.send('cant find product')}
+   
 
 })
